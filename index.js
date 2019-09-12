fc6c76a82470153d07a46bf30585ddd22a857f42
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2019-09-12 12:48:24 +0200

    made http server

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const ns = require('node-ns-api');
+const http = require('http');
+const moment = require('moment')
 const config = require('./config.json')
 const api = new ns.API(config.nskey);
-var parameters = {
-    dateTime: null,
-    maxJourneys: null,
-    lang: null,
-    station: "ZL",
-    uicCode: null,
-    source: null
-};
-api.getDepartures(parameters).then(board => {
-    let utrecht = board.departures.map(departure => {
-        if(departure.direction == "Utrecht Centraal")
-            return departure
+http.createServer(function(req, res){
+    console.log(req.headers)
+    var parameters = {
+        dateTime: req.headers.tijd,
+        maxJourneys: null,
+        lang: null,
+        station: req.headers.station,
+        uicCode: null,
+        source: null
+    };
+    api.getDepartures(parameters).then(board => {
+        let utrecht = board.departures.map(departure => {
+            if(departure.direction == req.headers.richting)
+                return departure
+        })
+
+        var filtered = utrecht.filter(function (el) {
+            return el != undefined;
+          });
+        res.write(JSON.stringify(filtered))
+        res.end();
+        //console.log(filtered)
     })
-    var filtered = utrecht.filter(function (el) {
-        return el != undefined;
-      });
-    console.log(filtered)
+}).listen(4000, '127.0.0.1', function(){
+
+
 })
